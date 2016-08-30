1d13a7e5d1f0ece2edf4b76231ad34e15b6759e5
Gaurav Sharma <hopegs.gs@gmail.com>
2016-08-30 19:39:58 +0530

    first toast

---
+++ app/src/main/java/com/example/itachi/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.itachi.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hi man!!", Toast.LENGTH_LONG).show();
+    }
+}
